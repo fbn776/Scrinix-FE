1762fb7b4956e507f72d94edb9098b6508496b6e
fbn776 <fbn776@gmail.com>
2024-11-04 13:00:22 +0530

    feat: faculty changes

--- src/components/FacultySearchPopup.tsx
+++ src/components/FacultySearchPopup.tsx
@@ -3,40 +3,29 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import {StateSetter} from "@/lib/types";
 import Dialog from "@mui/material/Dialog";
-import FacultyNameAutocomplete from "@/components/FacultySearch";
+import FacultyNameAutocomplete, {IFaculty} from "@/components/FacultySearch";
 
 function CreateExamForm({setOpen}: { setOpen: StateSetter<boolean> }) {
 
-    const handleClose = () => {
-        const confirm = window.confirm("Closing this dialog will discard all changes. Are you sure?");
-        if (confirm) setOpen(false);
-    };
 
-    const handleCreate = () => {
-
-    };
-
-    return <>
-        <div className="flex flex-col pt-4">
-            <FacultyNameAutocomplete/>
-            <div className='mt-4 flex justify-end gap-2'>
-                <Button onClick={handleClose}>Close</Button>
-                <Button onClick={handleCreate} autoFocus variant="contained">
-                    Assign
-                </Button>
-            </div>
-        </div>
-    </>
 }
 
 
-export default function FacultySearchPopup({open, setOpen}: {
+export default function FacultySearchPopup({open, setOpen, setSelectedFaculty}: {
     open: boolean,
     setOpen: StateSetter<boolean>,
+    setSelectedFaculty: StateSetter<IFaculty | null>
 }) {
     const handleClose = () => {
         const confirm = window.confirm("Closing this dialog will discard all changes. Are you sure?");
-        if (confirm) setOpen(false);
+        if (confirm) {
+            setOpen(false);
+            setSelectedFaculty(null);
+        }
+    };
+
+    const handleCreate = () => {
+        setOpen(false);
     };
 
     return (
@@ -48,9 +37,16 @@ export default function FacultySearchPopup({open, setOpen}: {
         >
             <DialogTitle>Assign Faculty</DialogTitle>
             <DialogContent className="w-[500px] text-center">
-                <CreateExamForm setOpen={setOpen}/>
+                <div className="flex flex-col pt-4">
+                    <FacultyNameAutocomplete setSelectedFaculty={setSelectedFaculty}/>
+                    <div className='mt-4 flex justify-end gap-2'>
+                        <Button onClick={handleClose}>Close</Button>
+                        <Button onClick={handleCreate} autoFocus variant="contained">
+                            Assign
+                        </Button>
+                    </div>
+                </div>
             </DialogContent>
         </Dialog>
     )
-        ;
 }
